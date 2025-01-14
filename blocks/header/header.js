e820c67f9ade42a5c85649b3fff8da60ef8f0872
Subash Selvaraj <sselvara@adobe.com>
2025-01-15 04:09:43 +0530

    fix for LCP

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -103,23 +103,6 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
-function initGTranslate() {
-  const script = document.createElement('script');
-  script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-  script.onload = () => {
-    if (typeof googleTranslateElementInit === 'function') {
-      window.googleTranslateElementInit();
-    }
-  };
-  script.async = true;
-  document.body.append(script);
-}
-
-window.googleTranslateElementInit = function () {
-  // eslint-disable-next-line no-new, no-undef
-  new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-};
-
 /**
  * loads and decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -179,10 +162,5 @@ export default async function decorate(block) {
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
-  const gTranslate = document.createElement('div');
-  // Set the id attribute
-  gTranslate.id = 'google_translate_element';
-  navWrapper.append(gTranslate);
-  initGTranslate();
   block.append(navWrapper);
 }
