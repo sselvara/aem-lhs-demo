85558bbdc0b549a2fd309db1801551226a523d9f
Subash Selvaraj <sselvara@adobe.com>
2025-01-15 04:50:10 +0530

    chat integration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -68,6 +68,16 @@ export function decorateMain(main) {
   decorateBlocks(main);
 }
 
+function loadChat() {
+  const s1 = document.createElement('script');
+  const s0 = document.getElementsByTagName('script')[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/6786efa4825083258e05083e/1ihjhg70t';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+  document.body.appendChild(s1);
+}
+
 /**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
@@ -90,6 +100,7 @@ async function loadEager(doc) {
   } catch (e) {
     // do nothing
   }
+  loadChat();
 }
 
 /**
