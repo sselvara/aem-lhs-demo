870b3f4f065ae1b00a74f2aba4ea3c30a6d70f21
Subash Selvaraj <sselvara@adobe.com>
2025-01-15 03:58:33 +0530

    TBT sample

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -103,6 +103,23 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
+function initGTranslate() {
+  const script = document.createElement('script');
+  script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+  script.onload = () => {
+    if (typeof googleTranslateElementInit === 'function') {
+      googleTranslateElementInit();
+    }
+  };
+  script.async = true;
+  document.body.append(script);
+}
+
+window.googleTranslateElementInit = function () {
+  // eslint-disable-next-line no-new, no-undef
+  new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+};
+
 /**
  * loads and decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -162,5 +179,10 @@ export default async function decorate(block) {
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
+  const gTranslate = document.createElement('div');
+  // Set the id attribute
+  gTranslate.id = 'google_translate_element';
+  navWrapper.append(gTranslate);
+  initGTranslate();
   block.append(navWrapper);
 }
