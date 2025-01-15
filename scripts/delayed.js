38c7df9f6e2b5cad49d8001686da628247bddf02
Subash Selvaraj <sselvara@adobe.com>
2025-01-15 09:52:57 +0530

    chat added in delay

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,11 @@
 // add delayed functionality here
-// function loadChat() {
-//   const s1 = document.createElement('script');
-//   const s0 = document.getElementsByTagName('script')[0];
-//   s1.src = 'https://embed.tawk.to/6786efa4825083258e05083e/1ihjhg70t';
-//   s1.setAttribute('crossorigin', '*');
-//   s1.async = true;
-//   s0.parentNode.insertBefore(s1, s0);
-//   document.body.appendChild(s1);
-// }
-// loadChat();
+function loadChat() {
+  const s1 = document.createElement('script');
+  const s0 = document.getElementsByTagName('script')[0];
+  s1.src = 'https://embed.tawk.to/6786efa4825083258e05083e/1ihjhg70t';
+  s1.setAttribute('crossorigin', '*');
+  s1.async = true;
+  s0.parentNode.insertBefore(s1, s0);
+  document.body.appendChild(s1);
+}
+loadChat();
