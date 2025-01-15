a37c00b152daef9749f749330232ac0a3c37a6ca
Subash Selvaraj <sselvara@adobe.com>
2025-01-15 09:56:30 +0530

    chat added in delay

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -68,15 +68,15 @@ export function decorateMain(main) {
   decorateBlocks(main);
 }
 
-// function loadChat() {
-//   const s1 = document.createElement('script');
-//   const s0 = document.getElementsByTagName('script')[0];
-//   s1.src = 'https://embed.tawk.to/6786efa4825083258e05083e/1ihjhg70t';
-//   s1.setAttribute('crossorigin', '*');
-//   s1.async = true;
-//   s0.parentNode.insertBefore(s1, s0);
-//   document.body.appendChild(s1);
-// }
+function loadChat() {
+  const s1 = document.createElement('script');
+  const s0 = document.getElementsByTagName('script')[0];
+  s1.src = 'https://embed.tawk.to/6786efa4825083258e05083e/1ihjhg70t';
+  s1.setAttribute('crossorigin', '*');
+  s1.async = true;
+  s0.parentNode.insertBefore(s1, s0);
+  document.body.appendChild(s1);
+}
 
 /**
  * Loads everything needed to get to LCP.
@@ -101,7 +101,7 @@ async function loadEager(doc) {
     // do nothing
   }
 
-  // loadChat();
+  loadChat();
 }
 
 /**
